1205ffd90b1bb16ae1af3737fd01c8d77001fca5
kalmansour <khaledalmansour24@gmail.com>
2020-10-19 19:17:14 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
 // Define the numbers and the operation here
+let firstNumber = 2;
+let secondNumber = 4;
 
+let operation = "+";
 // Write if-statements here
+
+if(operation === "+"){
+    console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+} else if(operation === "-"){
+    console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+} else if(operation === "*"){
+    console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+} else if(operation === "/"){
+    console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+}
